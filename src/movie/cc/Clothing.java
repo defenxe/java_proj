47c54f2be2d0c3417b00a5c03cd78bdae27b6045
sein0573 <144101162+sein0573@users.noreply.github.com>
2026-03-26 09:42:36 +0900

    아웃핏내용

---
+++ src/movie/cc/Clothing.java
@@ -0,0 +1,15 @@
+package movie.cc;
+
+public class Clothing {
+	
+	String customer; //고객번호
+	String type; // 상의, 하의, 신발
+	
+	//생성자
+	
+	public Clothing(String customer, String type){
+		this.customer=customer;
+		this.type=type;
+	}
+
+}
---
+++ src/movie/cc/User.java
@@ -0,0 +1,16 @@
+package movie.cc;
+
+public class User {
+	String gender;
+	int temp;
+	
+	
+	//생성자
+	
+	public User(String gender, int temp) {
+		super();
+		this.gender = gender;
+		this.temp = temp;
+	}
+
+}
