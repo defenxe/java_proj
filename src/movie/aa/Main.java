4ed2869bcd9af8d0e3d2618763ac1c398046c167
이용찬 <yong020504@gmail.com>
2026-03-26 10:50:20 +0900

    모든 클래스 합침

--- src/movie/aa/Main.java
+++ src/movie/aa/Main.java
@@ -5,6 +5,9 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+import movie.bbb.이동테스트;
+import movie.cc.OutfitMain;
+
  
 public class Main {
 	
@@ -12,6 +15,15 @@ public class Main {
 	
 	public static void main(String[] args) throws IOException {
 		
+		//옷입기 클래스 실행
+		OutfitMain.Outfit();
+		
+		//이동 클래스 실행
+		이동테스트.moveMain();
+		
+		
+		
+		//영화관 클래스 시작
 		영화관 movie = new 영화관();
 		
 		
