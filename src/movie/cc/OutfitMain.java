19fde2b3783cc20fd21497720b486c1c55d12778
DESKTOP-45CM6S3\user <kojyoun@gmail.com>
2026-03-27 12:32:29 +0900

    outfit interface 수정& 이동수단테스트->이동수단Main으로 수정

--- src/movie/cc/OutfitMain.java
+++ src/movie/cc/OutfitMain.java
@@ -5,9 +5,34 @@ import java.util.Scanner;
 public class OutfitMain {
 	
 	public static void Outfit() {
+		
 		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("\n== 영화관 예매 프로젝트 ==");
+		System.out.println("\n== 오늘의 outfit 추천 ==\n");
+		System.out.println("1. 날씨 2. 성별 3. 스타일");
+		System.out.println("선택:");
+		//System.out.print("성별 입력(남자/여자) : ");
+        int choice = sc.nextInt();
+        
+        Outfit selector = null;
 
-        System.out.print("성별 입력(남자/여자) : ");
+        if (choice == 1) {
+            selector = new WeatherBaseSelector();
+        } else if (choice == 2) {
+            selector = new GenderBaseSelector();
+        } else if (choice == 3) {
+        	selector = new StyleBaseSelector();    	
+        }else {
+            System.out.println("잘못된 선택입니다.");
+            return;
+        }
+        
+        selector.recommend();
+    }
+	
+}	
+/*		
         String gender = sc.next();
 
         System.out.print("현재 온도 입력 : ");
@@ -18,9 +43,9 @@ public class OutfitMain {
         OutfitService service = new OutfitService();
         service.setSelector(new WeatherBaseSelector());
         service.recommend(user);
-
+*/
         //sc.close();
-	}
+
 	
 	
 //    public static void main(String[] args) {
@@ -42,4 +67,3 @@ public class OutfitMain {
 //        sc.close();
 //    }
     
-}
--- src/movie/cc/WeatherBaseSelector.java
+++ src/movie/cc/WeatherBaseSelector.java
@@ -1,72 +1,39 @@
 package movie.cc;
 
+import java.util.Scanner;
+
 public class WeatherBaseSelector implements Outfit{
+	 
+
 	 @Override
-	    public Clothing[] selectOutfit(User user) {
+	 public void recommend() {
+		Scanner sc = new Scanner(System.in);
 
-	        if(user.gender.equals("남자")) {
+		System.out.print("현재 온도 입력: ");
+		int temp = sc.nextInt();
 
-	            if(user.temp <= 5) {
-	                return new Clothing[] {
-	                    new Clothing("아우터", "패딩"),
-	                    new Clothing("상의", "니트"),
-	                    new Clothing("하의", "기모바지"),
-	                    new Clothing("신발", "부츠")
-	                };
-	            } else if(user.temp <= 15) {
-	                return new Clothing[] {
-	                    new Clothing("아우터", "자켓"),
-	                    new Clothing("상의", "맨투맨"),
-	                    new Clothing("하의", "청바지"),
-	                    new Clothing("신발", "운동화")
-	                };
-	            } else if(user.temp <= 25) {
-	                return new Clothing[] {
-	                    new Clothing("상의", "반팔"),
-	                    new Clothing("하의", "면바지"),
-	                    new Clothing("신발", "스니커즈")
-	                };
-	            } else {
-	                return new Clothing[] {
-	                    new Clothing("상의", "민소매"),
-	                    new Clothing("하의", "반바지"),
-	                    new Clothing("신발", "샌들")
-	                };
-	            }
+		System.out.println("\n[날씨 기반 추천]");
 
-	        } else if(user.gender.equals("여자")) {
 
-	            if(user.temp <= 5) {
-	                return new Clothing[] {
-	                    new Clothing("아우터", "롱코트"),
-	                    new Clothing("상의", "니트"),
-	                    new Clothing("하의", "기모치마 or 바지"),
-	                    new Clothing("신발", "앵클부츠")
-	                };
-	            } else if(user.temp <= 15) {
-	                return new Clothing[] {
-	                    new Clothing("아우터", "가디건"),
-	                    new Clothing("상의", "블라우스"),
-	                    new Clothing("하의", "청바지 or 스커트"),
-	                    new Clothing("신발", "로퍼")
-	                };
-	            } else if(user.temp <= 25) {
-	                return new Clothing[] {
-	                    new Clothing("상의", "반팔 블라우스"),
-	                    new Clothing("하의", "스커트"),
-	                    new Clothing("신발", "플랫슈즈")
-	                };
-	            } else {
-	                return new Clothing[] {
-	                    new Clothing("상의", "나시"),
-	                    new Clothing("하의", "반바지 or 얇은 치마"),
-	                    new Clothing("신발", "샌들")
-	                };
-	            }
-	        }
-
-	        return new Clothing[] {
-	            new Clothing("오류", "성별 입력이 잘못되었습니다")
-	        };
-	    }
+		if (temp <=5) {
+		    System.out.println("아우터: 롱코트");
+		    System.out.println("상의: 니트");
+		    System.out.println("하의: 기모청바지");
+		    System.out.println("하의: 패딩부츠");
+		    } else if (temp <=15) {
+		    	System.out.println("아우터: 가디건");
+			    System.out.println("상의: 긴팔셔츠");
+			    System.out.println("하의: 청바지");
+			    System.out.println("하의: 로퍼");
+		    } else if (temp <=25) {		    	
+			    System.out.println("상의: 셔츠");
+			    System.out.println("하의: 면바지");
+			    System.out.println("하의: 스니커즈");
+		    }else {
+		    	System.out.println("상의: 반팔티셔츠");
+			    System.out.println("하의: 반바지");
+			    System.out.println("하의: 샌들");
+		    }
+		   
+	 }
 	}
